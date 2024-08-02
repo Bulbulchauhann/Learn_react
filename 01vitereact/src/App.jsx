2f543b615956ec3d91b96e5c4133ebaff9a5f1b2
Bulbul Singh Chauhan <chauhanbulbul8@gmail.com>
2024-08-03 00:49:50 +0530

    React Project with Vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+function App() {
+
+
+  return (
+    <>
+    <Chai />
+    <h1>Hello</h1>
+    <h3>blululululu</h3>
+    </>
+
+    //jsx can only export oonly one element at a time , so we use empty tags to wrap all html tags into that
+    
+  )
+}
+
+export default App
