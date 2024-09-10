93864db171c8ebe7606676fb10e9cdba6b679efb
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-09-10 10:55:38 +0530

    updates

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,10 +1,12 @@
-
+import useState from 'react';
 function App() {
-  const [color, setColor] = useState(0); //state
+  const [color, setColor] = useState("olive") //state
 
   return (
     <>
-    
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}>
+    </div>
     </>
   )
 }
